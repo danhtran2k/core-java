421f53a00843e7391d826e664b535762457c678e
danhtran2k <37130662+danhtran2k@users.noreply.github.com>
2018-03-22 20:55:05 +0700

    Add files via upload

---
+++ Java Basic Exercises -117 exercises with solution/Bai1.java
@@ -0,0 +1,13 @@
+package baitapki2;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+		System.out.println("Tran Cong Danh");
+		
+	}
+
+}
